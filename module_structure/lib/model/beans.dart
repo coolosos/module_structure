5d11668282e9237459ebdab55d48cef8eb96c4b2
Cayetano Bañón <cayetano.banon@gmail.com>
2024-03-16 18:29:27 +0100

    Added beans in model

---
+++ module_structure/lib/model/beans.dart
@@ -0,0 +1,12 @@
+/// Beans is an implementation of some functionality that are gona
+/// be used in all project. As:
+///
+/// * Launch Url
+/// * Open pdf
+/// * Share
+///
+/// This type of functionality usually need context for used or are
+/// NOT usecase functionalities.
+abstract class Beans {
+  const Beans();
+}
